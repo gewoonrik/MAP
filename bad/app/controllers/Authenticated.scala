87cc45da47def3efe0f48f88b00de45526ea774f
Rik Nijessen <riknijessen@gmail.com>
2016-01-03 13:20:32 +0100

    placed login fix into bad

--- bad/app/controllers/Authenticated.scala
+++ bad/app/controllers/Authenticated.scala
@@ -21,7 +21,7 @@ object Authenticated extends ActionBuilder[AuthenticatedRequest] {
 
 object AuthenticationUtils {
   def fromRequest(requestHeader: RequestHeader) =
-    requestHeader.session.get("id").flatMap(
-      id => User.findById(id.toInt)
+    requestHeader.session.get("username").flatMap(
+      username => User.findByUsername(username)
     )
 }
